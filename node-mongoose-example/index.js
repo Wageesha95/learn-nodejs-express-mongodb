e5c58044dbde9f7fb0cfbe72d7b5d13f44b3428a
Thilina <imwageesgha@gmail.com>
2020-06-20 18:54:08 +0530

    node-mongoose part 2:remove save() n use create()

--- node-mongoose-example/index.js
+++ node-mongoose-example/index.js
@@ -8,35 +8,26 @@ const connect = mongose.connect(url);
 connect.then((db) =>{
     console.log("connected to server correctly");
     
-    var newDish=Dishes({
+    Dishes.create({
         name:'Dhal',
         description:'test'
+    })
+    .then((dish)=>{
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)=>{
+        console.log(dishes)
+
+        return Dishes.remove({});
+    })
+    .then(()=>{
+        return mongose.connection.close();
+    })
+    .catch((err)=>{
+        console.log(err)
     });
-
-    var newDish1=Dishes({
-        name:'Dhal Currys',
-        description:'test'
-    });
-
-    newDish1.save()
-
-    newDish.save()
-        .then((dish)=>{
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes)=>{
-            console.log(dishes)
-
-           return Dishes.remove({});
-        })
-        .then(()=>{
-            return mongose.connection.close();
-        })
-        .catch((err)=>{
-            console.log(err)
-        });
 })
 .catch((err)=>{
     console.log(err)
